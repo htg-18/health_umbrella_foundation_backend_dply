1a2dcf0d7fbdd11c822688a879ec74d74cc4dbef
Manan Jain <mananjainoffi@gmail.com>
2023-04-03 03:32:30 +0530

    corrected the indetation of urls.py file and remote the if condition to add static and media url to urlpatterns

--- health_umbrella_foundation_backend/urls.py
+++ health_umbrella_foundation_backend/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
