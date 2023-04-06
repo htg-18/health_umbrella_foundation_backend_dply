a170bf669a9ded40e5dc3724547664b8df20acd4
Manan Jain <mananjainoffi@gmail.com>
2023-04-06 21:34:35 +0530

    added social media handles to the footer API results

--- footer/views.py
+++ footer/views.py
@@ -8,13 +8,19 @@ class FooterView(View):
         try:
             # final data to be sent as response
             final_data = {
-                "footer": {
+                "contactInformation": {
                     "contactEmail": footer_table.objects.get(key="contact_email_address").value,
                     "contactPhoneNumber": footer_table.objects.get(key="contact_phone_no").value,
                     "contactAddress": footer_table.objects.get(key="contact_address").value
+                },
+                "socialMediaInformation": {
+                    "instagramLink": footer_table.objects.get(key="instagram_link").value,
+                    "facebookLink": footer_table.objects.get(key="facebook_link").value,
+                    "youtubeLink": footer_table.objects.get(key="youtube_link").value,
+                    "twitterLink": footer_table.objects.get(key="twitter_link").value
                 }
             }
-            return JsonResponse(data=final_data, status=200)
+            return JsonResponse(data={"footer": final_data}, status=200)
         except:
             return JsonResponse(data={"message": "error while getting data"}, status=200)
 
