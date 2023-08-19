ffdedef07d014ff881f678f3e2f9ab52e7a94d90
Manan Jain <mananjainoffi@gmail.com>
2023-08-19 17:41:19 +0530

    analytics initialized

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import test_table
-
-
-admin.site.register(test_table)
--- analytics/models.py
+++ analytics/models.py
@@ -1,8 +1,2 @@
 from django.db import models
-from simple_history.models import HistoricalRecords
-
-class test_table(models.Model):
-    name = models.CharField(max_length=1000)
-    show = models.BooleanField()
-    history = HistoricalRecords()
 
