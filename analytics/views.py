68ff51c00cd89899c843ceb5d9e8aaa4732437bd
Manan Jain <mananjainoffi@gmail.com>
2023-10-23 19:12:31 +0530

    bug fix

--- analytics/views.py
+++ analytics/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 off = False
 
-def get_off(request):
+async def get_off(request):
     global off
     if off:
         return JsonResponse(data={}, status=200)
     else:
         return JsonResponse(data={}, status=400)
 
-def set_off(request):
+async def set_off(request):
     global off
     off = (not off)
     return JsonResponse(data={"off": off}, status=200)
