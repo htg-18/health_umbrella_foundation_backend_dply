bc0160f30c1fa7d97acdfe2db48f635c7339f006
Manan Jain <mananjainoffi@gmail.com>
2023-11-08 03:07:11 +0530

    minor test

--- analytics/urls.py
+++ analytics/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("get", views.get_off, name="get-off"),
     path("set", views.set_off, name="set-off"),
+    path("getg", views.get_offg, name="get-offg"),
+    path("setg", views.set_offg, name="set-offg"),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 off = False
+offg = False
 
 async def get_off(request):
     global off
@@ -13,3 +14,15 @@ async def set_off(request):
     global off
     off = (not off)
     return JsonResponse(data={"off": off}, status=200)
+
+async def get_offg(request):
+    global offg
+    if offg:
+        return JsonResponse(data={}, status=200)
+    else:
+        return JsonResponse(data={}, status=400)
+
+async def set_offg(request):
+    global offg
+    offg = (not offg)
+    return JsonResponse(data={"offg": off}, status=200)
