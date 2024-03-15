c6c40b63688358958b8e2b522c5a4e36e204e7af
Harsh Shah <115134783+htg-18@users.noreply.github.com>
2024-03-16 00:32:06 +0530

    Update settings.py

--- health_umbrella_foundation_backend/settings.py
+++ health_umbrella_foundation_backend/settings.py
@@ -178,8 +178,12 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 # CORS header settings
-CORS_ORIGIN_ALLOW_ALL = config("CORS_ORIGIN_ALLOW_ALL")
-CORS_ALLOW_CREDENTIALS = config("CORS_ALLOW_CREDENTIALS")
+# CORS_ORIGIN_ALLOW_ALL = config("CORS_ORIGIN_ALLOW_ALL")
+# CORS_ALLOW_CREDENTIALS = config("CORS_ALLOW_CREDENTIALS")
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
